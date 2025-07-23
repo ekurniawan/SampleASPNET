2308cffefd5dc0b976505806a807b4a6bfde5a73
Erick Kurniawan <erick.kurniawan@gmail.com>
2025-07-23 16:41:02 +0700

    menambahkan manual grid column

--- VBWebFormApplication/CustomerPage.aspx
+++ VBWebFormApplication/CustomerPage.aspx
@@ -4,11 +4,19 @@
     <div class="row">
         <div class="col-md-12">
             <h2>Customer</h2>
-
             <asp:Literal ID="ltMessage" runat="server" />
             <asp:Button ID="btnCustomer" Text="Get Data Customer" runat="server" CssClass="btn btn-primary"
-                OnClick="btnCustomer_Click"/><br />
-            <asp:GridView ID="gvCustomer" runat="server"></asp:GridView>
+                OnClick="btnCustomer_Click" /><br />
+            <asp:GridView ID="gvCustomer" AutoGenerateColumns="false" runat="server" CssClass="table table-striped">
+                <Columns>
+                    <asp:BoundField DataField="CustomerID" HeaderText="Customer ID" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" />
+                    <asp:BoundField DataField="CardID" HeaderText="CardID" />
+                    <asp:BoundField DataField="Address" HeaderText="Address" />
+                    <asp:BoundField DataField="PhoneNumber" HeaderText="Phone Number" />
+                    <asp:BoundField DataField="Email" HeaderText="Email" />
+                </Columns>
+            </asp:GridView>
         </div>
     </div>
 </asp:Content>
--- VBWebFormApplication/CustomerPage.aspx.vb
+++ VBWebFormApplication/CustomerPage.aspx.vb
@@ -11,26 +11,31 @@ Public Class CustomerPage
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         strConn = WebConfigurationManager.ConnectionStrings("AutomotiveDBConnectionString").ConnectionString
         conn = New SqlConnection(strConn)
+
+        LoadData()
     End Sub
 
-    Protected Sub btnCustomer_Click(sender As Object, e As EventArgs)
+    Private Sub LoadData()
         Try
             Dim strSql As String = "select * from Customer order by Name asc"
             cmd = New SqlCommand(strSql, conn)
             conn.Open()
-            Dim da As New 
+            Dim da As New SqlDataAdapter
             da.SelectCommand = cmd
             Dim ds As New DataSet
             da.Fill(ds, "Customer")
 
             gvCustomer.DataSource = ds.Tables("Customer")
             gvCustomer.DataBind()
-
         Catch ex As Exception
             ltMessage.Text = "<span class='alert alert-warning'>" & ex.Message & "</span>"
         Finally
             cmd.Dispose()
             conn.Close()
         End Try
+    End Sub
+
+    Protected Sub btnCustomer_Click(sender As Object, e As EventArgs)
+
     End Sub
 End Class
