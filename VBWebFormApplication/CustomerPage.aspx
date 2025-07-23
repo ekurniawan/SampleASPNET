e8b615881d0fe2f2d205cadb7aa2b919b29fe5ac
Erick Kurniawan <erick.kurniawan@gmail.com>
2025-07-23 16:11:26 +0700

    manual ADO NET

--- VBWebFormApplication/CustomerPage.aspx
+++ VBWebFormApplication/CustomerPage.aspx
@@ -4,6 +4,11 @@
     <div class="row">
         <div class="col-md-12">
             <h2>Customer</h2>
+
+            <asp:Literal ID="ltMessage" runat="server" />
+            <asp:Button ID="btnCustomer" Text="Get Data Customer" runat="server" CssClass="btn btn-primary"
+                OnClick="btnCustomer_Click"/><br />
+            <asp:GridView ID="gvCustomer" runat="server"></asp:GridView>
         </div>
     </div>
 </asp:Content>
--- VBWebFormApplication/CustomerPage.aspx.vb
+++ VBWebFormApplication/CustomerPage.aspx.vb
@@ -1,8 +1,36 @@
-﻿Public Class CustomerPage
+﻿Imports System.Data.SqlClient
+Imports System.Web.Configuration
+
+Public Class CustomerPage
     Inherits System.Web.UI.Page
 
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+    Private conn As SqlConnection
+    Private cmd As SqlCommand
+    Private strConn As String
 
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        strConn = WebConfigurationManager.ConnectionStrings("AutomotiveDBConnectionString").ConnectionString
+        conn = New SqlConnection(strConn)
     End Sub
 
+    Protected Sub btnCustomer_Click(sender As Object, e As EventArgs)
+        Try
+            Dim strSql As String = "select * from Customer order by Name asc"
+            cmd = New SqlCommand(strSql, conn)
+            conn.Open()
+            Dim da As New 
+            da.SelectCommand = cmd
+            Dim ds As New DataSet
+            da.Fill(ds, "Customer")
+
+            gvCustomer.DataSource = ds.Tables("Customer")
+            gvCustomer.DataBind()
+
+        Catch ex As Exception
+            ltMessage.Text = "<span class='alert alert-warning'>" & ex.Message & "</span>"
+        Finally
+            cmd.Dispose()
+            conn.Close()
+        End Try
+    End Sub
 End Class
