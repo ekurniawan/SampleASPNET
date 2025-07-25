008f1efe60133c3c055d34c9080d1b2de740394b
Erick Kurniawan <erick.kurniawan@gmail.com>
2025-07-25 10:00:26 +0700

    menambahkan bisnis logic layer

---
+++ SampleASPNET.BLL/CarBL.cs
@@ -0,0 +1,57 @@
+﻿using SampleASPNET.BLL.DTO;
+using SampleASPNET.BO;
+using SampleASPNET.DAL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleASPNET.BLL
+{
+    public class CarBL : ICarBL
+    {
+        private ICar carDAL;
+        public CarBL()
+        {
+            carDAL = new CarDAL();
+        }
+
+        public void AddCar(CarDTO car)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCar(int carID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CarDTO> GetAllCars()
+        {
+            List<CarDTO> carDtos = new List<CarDTO>();
+            var results = carDAL.GetAll();
+            foreach (var car in results)
+            {
+                carDtos.Add(new CarDTO
+                {
+                    CarID = car.CarID,
+                    Model = car.Model,
+                    Type = car.Type,
+                    BasePrice = car.BasePrice,
+                    Color = car.Color,
+                    Stock = car.Stock
+                });
+            }
+            return carDtos;
+        }
+
+        public CarDTO GetCarByID(int carID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCar(CarDTO car)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SampleASPNET.BLL/ICarBL.cs
@@ -0,0 +1,37 @@
+﻿using SampleASPNET.BLL.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleASPNET.BLL
+{
+    public interface ICarBL
+    {
+        /// <summary>
+        /// Gets all cars.
+        /// </summary>
+        /// <returns>A list of car DTOs.</returns>
+        List<CarDTO> GetAllCars();
+        /// <summary>
+        /// Gets a car by its ID.
+        /// </summary>
+        /// <param name="carID">The ID of the car.</param>
+        /// <returns>A car DTO if found, otherwise null.</returns>
+        CarDTO GetCarByID(int carID);
+        /// <summary>
+        /// Adds a new car.
+        /// </summary>
+        /// <param name="car">The car DTO to add.</param>
+        void AddCar(CarDTO car);
+        /// <summary>
+        /// Updates an existing car.
+        /// </summary>
+        /// <param name="car">The car DTO with updated information.</param>
+        void UpdateCar(CarDTO car);
+        /// <summary>
+        /// Deletes a car by its ID.
+        /// </summary>
+        /// <param name="carID">The ID of the car to delete.</param>
+        void DeleteCar(int carID);
+    }
+}
